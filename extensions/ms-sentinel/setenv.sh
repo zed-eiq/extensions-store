9bef4c541b7e643c26126b1ae221eea2f0112453
pAndaCry123 <114917169+pAndaCry123@users.noreply.github.com>
2022-12-21 14:43:42 +0100

    MS-Sentinel outgoing feed

---
+++ extensions/ms-sentinel/setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export PACKAGE_NAME="ms-sentinel"
+export VERSION="0.0.1"
+export MAINTAINER="John Doe <email@example.com>"
+export DESCRIPTION="Short description of the extension you're building"
